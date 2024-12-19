c6f85e0e3ec74effd49d1cd85e4721334015138d
Joshua <joshua.ghattas@gmail.com>
2024-12-19 16:30:59 -0600

    Fixed problem with font

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 
-export const jetbrains_800weight = JetBrains_Mono({
+const jetbrains_800weight = JetBrains_Mono({
   weight: "800",
   subsets: ["latin"],
 });
