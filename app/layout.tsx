dd7184a1978d14898e10e1c60ea0bf43a8a4333a
Joshua <joshua.ghattas@gmail.com>
2024-12-19 18:07:52 -0600

    Made the "coming soon" page more interesting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Convoes 2",
@@ -25,9 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-blue-600 antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className={"text-5xl" + " " + jetbrains_800weight.className}>
-          Convoes 2 - Coming Soon!
-        </h1>
+        <div className="p-[200px] border-white border-2 border-solid rounded-[100px]">
+          <h1 className={"text-5xl" + " " + jetbrains_800weight.className}>
+            Convoes 2 - Coming Soon!
+          </h1>
+        </div>
       </main>
     </div>
   );
