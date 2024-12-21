45bfcff16ad75d1b186653295a96bc430571b20e
Joshua <joshua.ghattas@gmail.com>
2024-12-21 15:05:59 -0600

    Fixed env var problem

--- app/allusers/page.tsx
+++ app/allusers/page.tsx
@@ -7,8 +7,10 @@ const jetbrains_400weight = JetBrains_Mono({
     subsets: ["latin"],
 });
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://internetbowser.com"
+
 async function fetchUsers() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`);
+    const response = await fetch(`${baseUrl}/api/users`);
     if (!response.ok) throw new Error("Failed to fetch users");
     return response.json();
 }
