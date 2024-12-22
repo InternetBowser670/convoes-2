00a0bda95cafac1dd2491e39c5891b2567bce8bd
Joshua <joshua.ghattas@gmail.com>
2024-12-22 06:35:24 -0600

    0.6.1 - Optimized the users page

--- app/allusers/page.tsx
+++ app/allusers/page.tsx
@@ -1,48 +1,69 @@
-import { JetBrains_Mono } from "next/font/google";
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import { User } from "@/src/lib/types";
-import React, { Suspense } from "react";
+import { JetBrains_Mono } from "next/font/google";
 
 const jetbrains_400weight = JetBrains_Mono({
     weight: "400",
     subsets: ["latin"],
 });
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://convoes-2.internetbowser.com"
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://convoes-2/internetbowser.com"
 
-async function fetchUsers() {
-    const response = await fetch(`${baseUrl}/api/users`);
-    return response.json();
-}
+export default function Page() {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-async function UsersList() {
-    const users = await fetchUsers();
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch(`${baseUrl}/api/users`);
+                const data = await response.json();
+                setData(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        <div className={`grid grid-cols-3 ${jetbrains_400weight.className}`}>
-            {users.map((user: User) => (
-                <div key={user._id} className="bg-blue-700 border-white border-2 rounded-2xl w-[500] m-3 p-3 shrink">
-                    <div className="flex items-center content-center">
-                        <img
-                            src={
-                                user.image_url ||
-                                "https://img.clerk.com/eyJ0eXBlIjoiZGVmYXVsdCIsImlpZCI6Imluc18ycGw2MWhCQlZzTEJPeVpPMDJmUkQ2TExoQzQiLCJyaWQiOiJ1c2VyXzJxWDQyMERNZno2RHFnbkJTckQxWGowbFpxUiIsImluaXRpYWxzIjoiQVgifQ"
-                            }
-                            className="rounded-full pfp"
-                            alt={`${user.username}'s pfp`}
-                        />
-                        <p>​ @{user.username}</p>
-                    </div>
-                    <br />
-                    {user.first_name ? (
-                        <p>
-                            Name: {user.first_name} {user.last_name}
-                        </p>
-                    ) : (
-                        <p>Name: (N/A)</p>
-                    )}
-                    <br />
+        <div className={`${jetbrains_400weight.className}`}>
+            {isLoading ? (
+                <UsersFallback/>
+            ) : (
+                <div className={`grid grid-cols-3`}>
+                    {data.map((user: User) => (
+                        <div key={user._id} className="bg-blue-700 border-white border-2 rounded-2xl w-[500] m-3 p-3 shrink">
+                            <div className="flex items-center content-center">
+                                <img
+                                    src={
+                                        user.image_url ||
+                                        "https://img.clerk.com/eyJ0eXBlIjoiZGVmYXVsdCIsImlpZCI6Imluc18ycGw2MWhCQlZzTEJPeVpPMDJmUkQ2TExoQzQiLCJyaWQiOiJ1c2VyXzJxWDQyMERNZno2RHFnbkJTckQxWGowbFpxUiIsImluaXRpYWxzIjoiQVgifQ"
+                                    }
+                                    className="rounded-full pfp"
+                                    alt={`${user.username}'s pfp`}
+                                />
+                                <p>​ @{user.username}</p>
+                            </div>
+                            <br />
+                            {user.first_name ? (
+                                <p>
+                                    Name: {user.first_name} {user.last_name}
+                                </p>
+                            ) : (
+                                <p>Name: (N/A)</p>
+                            )}
+                            <br />
+                        </div>
+                    ))}
                 </div>
-            ))}
+            )}
         </div>
     );
 }
@@ -61,25 +82,17 @@ function UsersFallback() {
 }
 
 function TemplateUser() {
-    return(<div className="bg-blue-700 border-white border-2 skeleton rounded-2xl w-[500] m-3 p-3 shrink">
+    return (<div className="bg-blue-700 border-white border-2 skeleton rounded-2xl w-[500] m-3 p-3 shrink">
         <div className="flex items-center content-center">
             <div className="rounded-full skeleton-avatar bg-red-500 pfp">
                 <div className="glimmer-line"></div>
             </div>
             <div className="w-[100] ml-2 rounded-xl skeleton-title bg-red-500 h-[25]">
-                </div>
+            </div>
         </div>
         <br />
         <div className="w-[300] ml-2 rounded-xl skeleton-text bg-red-500 h-[25]">
         </div>
         <br />
     </div>)
-}
-
-export default function Page() {
-    return (
-        <Suspense fallback={<UsersFallback />}>
-            <UsersList />
-        </Suspense>
-    );
 }
