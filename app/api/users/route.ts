e639b3108664a4e9a66959c800c678446d20f4d5
Joshua Ghattas <102930366+InternetBowser670@users.noreply.github.com>
2025-01-15 13:57:54 -0600

    Update route.ts to sort by username and priority

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -26,7 +26,16 @@ export async function GET() {
     db = client.db(process.env.MONGODB_DB_NAME);
   }
   const users = db.collection("users");
-  const allUsers = await users.find({}).sort({ priority: -1 }).toArray();
+  const allUsers = await users.find({}).sort((a, b) => {
+  let retval = 0;
+  if (a.priority > b.priority)
+    retval = -1;
+  if (a.priority < b.priority)
+    retval = 1;
+  if (retval === 0)
+    retval = a.username < b.username ? -1 : 1;
+  return retval;
+}}).toArray();
 
   const serializedUsers = allUsers.map(user => ({
     _id: user._id.toString(),
